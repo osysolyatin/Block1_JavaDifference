2e0b916c0a795b0439436da52d0d232061cd1ed5
Oleg Sysolyatin <osysolyatin@gmail.com>
2022-03-08 03:10:36 +0300

    Amended Task 1 and Task 3

--- src/main/kotlin/TaskFirst.kt
+++ src/main/kotlin/TaskFirst.kt
@@ -1,12 +1,16 @@
+const val MAX_MONTH_LIMIT = 600_000
+const val MIN_COMMISSION = 35
+const val TRANSACTION_COMMISSION = 0.0075
+
 fun main() {
     var amountPerMonth = 0
-    val minCommission: Double = 35.0
 
-    if (amountPerMonth <= 600_000) {
+    if (amountPerMonth <= MAX_MONTH_LIMIT) {
         val amount = 100_000
-        val commission = if (amount * 0.0075 > minCommission) amount * 0.0075 else minCommission
-        println("Размер комиссии составит: $commission рублей")
-        println("Суммарный платеж, с учетом комиссии составит: " + (amount + commission))
+        val commission =
+            if (amount * 100 * TRANSACTION_COMMISSION > MIN_COMMISSION * 100) (amount * 100 * TRANSACTION_COMMISSION).toInt() else (MIN_COMMISSION * 100)
+        println("Размер комиссии составит: ${commission / 100} рублей")
+        println("Суммарный платеж, с учетом комиссии составит: ${(amount + (commission / 100))} рублей")
         amountPerMonth += amount
     } else println("Вы превысили месячный лимит переводов")
 }
--- src/main/kotlin/TaskThird.kt
+++ src/main/kotlin/TaskThird.kt
@@ -1,21 +1,25 @@
+const val ITEM_PRICE = 1_000U
+const val DISCOUNT_SUM_START = 1_000U
+const val DISCOUNT_SUM_IN_PERCENT_START = 10_000U
+const val DISCOUNT_BASIS = 100U
+const val DISCOUNT_IN_PERCENT = 5U
+const val DISCOUNT_MUSIC_LOVER = 1U
+
 fun main() {
-    val itemPrice = 1_000
-    val itemCount = 11
-    val discount = 100
-    val discountInPercent = 0.05
-    val discountStart = 1_000
-    val discountPercentStart = 10_000
-    var numberOfPurchasePerMonth = 1 //кол-во покупок в месяц
+    print("Введите кол-во дисков, которое хотите купить (например 11) : ")
+    val itemCount = readLine()?.toUInt() ?: return
+    var numberOfPurchasePerMonth = 1 //кол-во покупок в текущем месяце
 
-    val totalPrice = itemPrice * itemCount
+    val totalPrice = ITEM_PRICE * itemCount * 100U
     numberOfPurchasePerMonth++ // после покупки инкреминтируем кол-во покупок
-    val isMusicLover = numberOfPurchasePerMonth > 1 // проверка на "меломана"
-    var result = if (totalPrice > discountPercentStart) {
-        totalPrice - (totalPrice * discountInPercent)
-    } else if (totalPrice > discountStart) {
-        (totalPrice - discount).toDouble()
-    } else totalPrice.toDouble()
+    val isMusicLover = numberOfPurchasePerMonth > 1 // проверка на "меломана" (true/false)
 
-    result = if (isMusicLover) result - (result * 0.01) else result // доп.скидка за меломанство
-    println("Total Price: $result")
+    var result = if (totalPrice > DISCOUNT_SUM_IN_PERCENT_START * 100U) {
+        totalPrice - (totalPrice * DISCOUNT_IN_PERCENT / 100U)
+    } else if (totalPrice > DISCOUNT_SUM_START * 100U) {
+        (totalPrice - DISCOUNT_BASIS * 100U)
+    } else totalPrice
+
+    result = if (isMusicLover) result - (result * DISCOUNT_MUSIC_LOVER / 100U) else result // доп.скидка за меломанство
+    println("Сумма покупки с учетом скидок составит:  ${result / 100U} рублей ${result % 100U} копеек")
 }
