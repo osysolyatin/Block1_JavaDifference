8d58f08f987bc3e0d5dfdded166523060ee93c42
Oleg Sysolyatin <osysolyatin@gmail.com>
2022-03-14 22:53:02 +0300

    Задание 2 - склонение 111 людям

--- src/main/kotlin/TaskSecond.kt
+++ src/main/kotlin/TaskSecond.kt
@@ -1,5 +1,5 @@
 fun main() {
-    val likes = 10
+    val likes = 111
     val phraseEnds = if (likes % 10 == 1 && likes % 100 != 11) "человеку" else "людям"
     println("Понравилось $likes $phraseEnds")
 }
